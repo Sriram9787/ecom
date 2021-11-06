f7e86f82fb1e95ba558bb9373d31f9c9145d87ed
sriram <rsriram.rthoughts@gmail.com>
2021-11-06 13:37:29 +0530

    till checkout

---
+++ src/data.js
@@ -0,0 +1,75 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://image.freepik.com/free-photo/portrait-young-happy-girl-holding-shopping-bags_171337-2608.jpg",
+        title: "SUMMER SALE",
+        desc: "DONT COMPROMISE ON STYLE GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://image.freepik.com/free-photo/portrait-young-happy-girl-holding-shopping-bags_171337-2608.jpg",
+        title: "WINTER SALE",
+        desc: "DONT COMPROMISE ON STYLE GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        img: "https://image.freepik.com/free-photo/portrait-young-happy-girl-holding-shopping-bags_171337-2608.jpg",
+        title: "HOT SALE",
+        desc: "DONT COMPROMISE ON STYLE GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "fbf0f4"
+    },
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://image.freepik.com/free-photo/portrait-handsome-smiling-stylish-young-man-model-dressed-red-checkered-shirt-fashion-man-posing_158538-4909.jpg",
+        title: "SHIRT STYLE"
+    },
+    {
+        id: 2,
+        img: "https://image.freepik.com/free-photo/men-s-ankle-sneakers-white-street-style-apparel-shoot_53876-119733.jpg",
+        title: "LOUNGEWEAR LOVE",
+        
+    },
+    {
+        id: 3,
+        img: "https://image.freepik.com/free-photo/young-man-with-hands-his-pocket-looking-away_23-2148096484.jpg",
+        title: "LIGHT JACKETS"
+    },
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://image.freepik.com/free-photo/fashion-polo-shirt-men_1203-7534.jpg",
+    },
+    {
+        id: 2,
+        img: "https://image.freepik.com/free-photo/casual-men-short-pants_1203-8171.jpg",
+        
+        
+    },
+    {
+        id: 3,
+        img: "https://image.freepik.com/free-photo/front-back-isolated-black-t-shirts_125540-1092.jpg",
+       
+    },
+    {
+        id: 1,
+        img: "https://image.freepik.com/free-photo/fashion-polo-shirt-men_1203-7534.jpg",
+    },
+    {
+        id: 2,
+        img: "https://image.freepik.com/free-photo/casual-men-short-pants_1203-8171.jpg",
+        
+        
+    },
+    {
+        id: 3,
+        img: "https://image.freepik.com/free-photo/front-back-isolated-black-t-shirts_125540-1092.jpg",
+       
+    },
+]
